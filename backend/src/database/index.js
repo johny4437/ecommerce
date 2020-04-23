ed2349d42afb7d43ff49bbc64b4899730dbfeab7
johny anastacio <johnyanastaciods@gmail.com>
2020-04-22 21:13:13 -0300

    change

--- backend/src/database/index.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-require('dotenv').config({path:'./src/config/.env'});
-const uri = process.env.URI; 
-
-
-mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
-
-module.exports = mongoose;
--- backend/src/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const route = express.Router();
-
-const bookController = require('./controllers/bookController');
-
-route.get('/books', bookController.index);
-route.post('/books', bookController.store);
-
-module.exports= route;
