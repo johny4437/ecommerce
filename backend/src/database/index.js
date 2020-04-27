5e262fd9806240fdb1085a3efe24373901dd1451
johny anastacio <johnyanastaciods@gmail.com>
2020-04-26 22:00:59 -0300

    backend removed

--- backend/src/database/index.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-require('dotenv').config({path:'./src/config/.env'});
-const uri = "mongodb+srv://ecommerce:vanhelsing123@cluster0-hyyla.mongodb.net/test?retryWrites=true&w=majorit";
-
-
-mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
-
-module.exports = mongoose;
